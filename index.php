17c69a047b24de2bb5a0796a62a1f108d793965d
krm-shrftdnv <shkar2001@mail.ru>
2022-04-19 17:50:29 +0300

    err message

--- index.php
+++ index.php
@@ -6,8 +6,10 @@ define("CORRECT_PASSWORD", getenv('PASSWORD'));
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['password']) && $_POST['password'] === CORRECT_PASSWORD) {
         include 'dr.html';
-        return;
+    } else {
+        include 'form_err.html';
     }
+    return;
 }
 
 include 'form.html';

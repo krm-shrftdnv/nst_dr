1f79bba42cd74c15501c13ea56fb6260c9a033f2
krm-shrftdnv <shkar2001@mail.ru>
2022-04-19 19:23:06 +0300

    styles

--- index.php
+++ index.php
@@ -3,13 +3,18 @@
 //putenv('PASSWORD=test');
 define("CORRECT_PASSWORD", getenv('PASSWORD'));
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['password']) && $_POST['password'] === CORRECT_PASSWORD) {
-        include 'dr.html';
-    } else {
-        include 'form_err.html';
+if (in_array($_SERVER['REQUEST_URI'], ['/', '/form'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['password']) && $_POST['password'] === CORRECT_PASSWORD) {
+            echo file_get_contents('dr.html');
+        } else {
+            echo file_get_contents('form_err.html');
+        }
+        return;
     }
-    return;
+    echo file_get_contents('form.html');
+} else {
+    $filename = trim($_SERVER['REQUEST_URI'], '/');
+    header('content-type', mime_content_type($filename));
+    echo file_get_contents($filename);
 }
-
-include 'form.html';

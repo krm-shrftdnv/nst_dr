d8788f1417a5bbbe1425b9e2da0f2c5622d591b9
krm-shrftdnv <shkar2001@mail.ru>
2022-04-19 16:05:57 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+putenv('PASSWORD=test');
+define("CORRECT_PASSWORD", getenv('PASSWORD'));
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['password']) && $_POST['password'] === CORRECT_PASSWORD) {
+        include 'dr.html';
+        return;
+    }
+}
+
+include 'form.html';

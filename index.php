ff47e85e905e54f14b37d7e7c35debff238b967c
krm-shrftdnv <shkar2001@mail.ru>
2022-04-19 19:46:48 +0300

    action

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //putenv('PASSWORD=test');
 define("CORRECT_PASSWORD", getenv('PASSWORD'));
 
-if (in_array($_SERVER['REQUEST_URI'], ['/', '/form'])) {
+if (in_array($_SERVER['REQUEST_URI'], ['/', '/index.php'])) {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['password']) && $_POST['password'] === CORRECT_PASSWORD) {
             echo file_get_contents('dr.html');
